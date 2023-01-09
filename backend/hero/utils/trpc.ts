417f767359078243bcdd30f09573c509dbf1b223
tusheer <jaanealamtusher@gmail.com>
2023-01-09 11:32:18 +0600

    fix middleware initialize issue

--- backend/hero/utils/trpc.ts
+++ backend/hero/utils/trpc.ts
@@ -1,8 +1,7 @@
-import { inferAsyncReturnType, initTRPC } from '@trpc/server';
+import { TRPCError, inferAsyncReturnType, initTRPC } from '@trpc/server';
 import superjson from 'superjson';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { ZodError } from 'zod';
-import { isAuthed } from '../middleware';
 
 export const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({ req, res });
 
@@ -22,10 +21,21 @@ const t = initTRPC.context<Context>().create({
     },
 });
 
-export const router = t.router;
-
 export const middleware = t.middleware;
+
+export const isAuthed = middleware(({ next, ctx }) => {
+    if (!ctx?.req.headers.token) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' });
+    }
+    return next({
+        ctx: {
+            user: ctx.req.headers.token,
+        },
+    });
+});
 
 export const publicProcedure = t.procedure;
 
 export const protectedProcedure = t.procedure.use(isAuthed);
+
+export const router = t.router;
