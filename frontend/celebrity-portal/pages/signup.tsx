101c61c2992858d9040c97a39c6a0389606757db
tusheer <jaanealamtusher@gmail.com>
2023-01-15 22:43:02 +0600

    signup celebrity page done

--- frontend/celebrity-portal/pages/signup.tsx
+++ frontend/celebrity-portal/pages/signup.tsx
@@ -9,15 +9,15 @@ type CelebritySignupType = z.infer<typeof celebritySignupSchema>;
 
 const SignupPage = () => {
     const {
-        // register,
+        register,
         handleSubmit,
-        formState: { isValid },
+        formState: { isValid, errors },
     } = useForm<CelebritySignupType>({
         resolver: zodResolver(celebritySignupSchema),
-        mode: 'onChange',
+        mode: 'onSubmit',
     });
 
-    const handleOnSubmit: SubmitHandler<CelebritySignupType> = () => {
+    const handleOnSubmit: SubmitHandler<CelebritySignupType> = (data) => {
         if (!isValid) {
             return;
         }
@@ -26,10 +26,44 @@ const SignupPage = () => {
     return (
         <div className="min-h-screen pt-20 px-5 bg-gray-50">
             <div className="max-w-2xl block p-7 bg-white border rounded-md mx-auto">
-                <h1 className="text-3xl mb-8 font-medium">Signup celebrity</h1>
+                <h1 className="text-3xl mb-8 text-gray-800 font-medium">Signup celebrity</h1>
                 <form onSubmit={handleSubmit(handleOnSubmit)}>
-                    <TextInput className="mb-2.5" label="First Name" />
-                    <TextInput label="Second Name" />
+                    <TextInput
+                        error={!!errors.firstName}
+                        errorText={errors.firstName?.message}
+                        {...register('firstName')}
+                        className="mb-2.5"
+                        label="First Name"
+                    />
+                    <TextInput
+                        error={!!errors.lastName}
+                        errorText={errors.lastName?.message}
+                        {...register('lastName')}
+                        label="Second Name"
+                        className="mb-2.5"
+                    />
+                    <TextInput
+                        error={!!errors.password}
+                        errorText={errors.phoneNumber?.message}
+                        {...register('phoneNumber')}
+                        label="Phone Number"
+                        className="mb-2.5"
+                    />
+                    <TextInput
+                        error={!!errors.email}
+                        errorText={errors.email?.message}
+                        {...register('email')}
+                        label="Email"
+                        className="mb-2.5"
+                    />
+                    <TextInput
+                        error={!!errors.password}
+                        errorText={errors.password?.message}
+                        {...register('password')}
+                        type="password"
+                        label="Password"
+                        className="mb-2.5"
+                    />
                     <button
                         className="mt-7 bg-gray-800 rounded-md h-10 text-white text-sm font-medium px-7 hover:bg-gray-900"
                         type="submit"
--- packages/ui/components/TextInput/index.tsx
+++ packages/ui/components/TextInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes, useId } from 'react';
+import React, { InputHTMLAttributes, forwardRef, useId } from 'react';
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
     className?: string;
@@ -13,41 +13,36 @@ interface IInputProps extends InputHTMLAttributes<HTMLInputElement | HTMLTextAre
     startIcon?: React.ReactElement;
 }
 
-export const TextInput: React.FC<IInputProps> = ({
-    type = 'text',
-    textArea,
-    errorText,
-    label,
-    className = '',
-    rows = 2,
-    error,
-    name = '',
-    ...rest
-}) => {
-    const uid = useId();
+export const TextInput: React.FC<IInputProps> = forwardRef<HTMLInputElement, IInputProps>(
+    ({ type = 'text', textArea, errorText, label, className = '', rows = 2, error, name = '', ...rest }, ref) => {
+        const uid = useId();
 
-    return (
-        <div className={className}>
-            <label htmlFor={uid + name} className="block mb-2 text-sm font-medium text-gray-900  dark:text-white">
-                {label}
-            </label>
-            {textArea ? (
-                <textarea {...rest} rows={rows}></textarea>
-            ) : (
-                <input
-                    type={type}
-                    id={uid + name}
-                    name={name}
-                    aria-invalid={error}
-                    className="bg-gray-50 border border-gray-300 outline-0 text-gray-900 text-sm rounded-md focus:ring-1 focus:ring-gray-500   focus:border-gray-500 block w-full px-2.5 py-1 h-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
-                    {...rest}
-                />
-            )}
-            {errorText && error ? (
-                <small className="text-red-600 text-sm" role="alert">
-                    {errorText}
-                </small>
-            ) : null}
-        </div>
-    );
-};
+        return (
+            <div className={className}>
+                <label htmlFor={uid + name} className="block mb-2 text-sm font-medium text-gray-900  dark:text-white">
+                    {label}
+                </label>
+                {textArea ? (
+                    <textarea {...rest} rows={rows}></textarea>
+                ) : (
+                    <input
+                        ref={ref}
+                        type={type}
+                        id={uid + name}
+                        name={name}
+                        aria-invalid={error}
+                        className="bg-gray-50 border border-gray-300 outline-0 text-gray-900 text-sm rounded-md focus:ring-1 focus:ring-gray-500   focus:border-gray-500 block w-full px-2.5 py-1 h-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                        {...rest}
+                    />
+                )}
+                {errorText && error ? (
+                    <small className="text-red-600 text-sm" role="alert">
+                        {errorText}
+                    </small>
+                ) : null}
+            </div>
+        );
+    }
+);
+
+TextInput.displayName = 'TextInput';
