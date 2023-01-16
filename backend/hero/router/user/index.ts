90f0a28410285c745a9e8ad000a514bb1f3c033d
tusheer <jaanealamtusher@gmail.com>
2023-01-16 23:53:25 +0600

    jwt add in hero

--- backend/hero/router/user/index.ts
+++ backend/hero/router/user/index.ts
@@ -2,23 +2,33 @@ import { celebritySignupSchema } from 'schema';
 import { router, publicProcedure } from '../../utils/trpc';
 import prisma from '../../utils/prisma';
 import { TRPCError } from '@trpc/server';
+import argon2 from 'argon2';
+import jwt from 'jsonwebtoken';
 
 const userRoute = router({
     createCelebrityUser: publicProcedure
         .input(celebritySignupSchema)
         .mutation(async ({ input: { email, firstName, lastName, password, phoneNumber } }) => {
             try {
+                const hashpassword = await argon2.hash(password);
+
                 const celebrityUser = await prisma.celebrity.create({
                     data: {
                         email,
                         firstName,
                         lastName,
-                        password,
+                        password: hashpassword,
                         phone: Number(phoneNumber),
                     },
+                    select: {
+                        email: true,
+                        id: true,
+                        username: true,
+                    },
                 });
-                console.log({ email });
-                return celebrityUser;
+                const token = jwt.sign({ ...celebrityUser }, process.env.JWT_SECRET as string, { algorithm: 'RS256' });
+
+                return { ...celebrityUser, token };
             } catch (error) {
                 throw new TRPCError({
                     cause: error,
