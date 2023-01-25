0722350a1474049b8b82a2b5e263cc3206170c47
tusheer <jaanealamtusher@gmail.com>
2023-01-25 15:32:57 +0600

    seferate all login in service

--- backend/hero/controller/user/index.ts
+++ backend/hero/controller/user/index.ts
@@ -1,33 +1,10 @@
 import { CelebritySignupType, SigninType } from 'schema';
-import prisma, { Prisma } from '../../utils/prisma';
 import { TRPCError } from '@trpc/server';
 import argon2 from 'argon2';
 import { Context } from '../../server';
-import { CookieOptions } from 'express';
-import customConfig from '../../config/default';
-import { signJwt } from '../../utils/jwt';
-import redisClient from '../../utils/connectRedis';
 import { nanoid } from 'nanoid';
 import useragent from 'express-useragent';
-// [...] Cookie options
-
-const cookieOptions: CookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
-};
-
-const accessTokenCookieOptions = {
-    ...cookieOptions,
-    expires: new Date(Date.now() + customConfig.accessTokenExpiresIn * 24 * 3600000),
-};
-
-const refreshTokenCookieOptions = {
-    ...cookieOptions,
-    expires: new Date(Date.now() + customConfig.refreshTokenExpiresIn * 24 * 3600000),
-};
-
-//TODO : Add lodash for utils
+import userServices from '../../service/user';
 
 const stringReplace = (str: string) => str.replace(' ', '-').toLowerCase();
 
@@ -39,9 +16,9 @@ export const createCelebrityUserController = async ({
     ctx: Context;
 }) => {
     try {
-        const _user = await prisma.celebrity.findUnique({
+        const _user = await userServices.findCelebrityUser({
             where: {
-                email: email,
+                email,
             },
         });
 
@@ -53,10 +30,11 @@ export const createCelebrityUserController = async ({
         }
         const deviceInfo = useragent.parse(ctx.req.headers['user-agent'] as string);
 
+        //TODO : Add lodash for utils
         const username = `${stringReplace(firstName)}-${stringReplace(lastName)}-${nanoid(6)}`;
         const hashpassword = await argon2.hash(password);
 
-        const celebrityUser = await prisma.celebrity.create({
+        const celebrityUser = await userServices.createCelebrityUser({
             data: {
                 email,
                 firstName,
@@ -68,28 +46,22 @@ export const createCelebrityUserController = async ({
         });
 
         // Create the Access and refresh Tokens
-        const { access_token, refresh_token } = signTokens(celebrityUser);
+        const { access_token, refresh_token } = userServices.signTokens(celebrityUser);
 
-        const session = await prisma.session.create({
+        const session = await userServices.createUserSession({
             data: {
                 refreshToken: refresh_token,
                 userAgent: JSON.stringify(deviceInfo),
                 celebrityId: celebrityUser.id,
             },
         });
-
-        ctx.res.cookie('access_token', access_token, accessTokenCookieOptions);
-        ctx.res.cookie('refresh_token', refresh_token, refreshTokenCookieOptions);
-        ctx.res.cookie('device_uid', session.id, {
-            ...cookieOptions,
-            httpOnly: true,
-        });
-        ctx.res.cookie('logged_in', true, {
-            ...cookieOptions,
-            httpOnly: false,
+        userServices.setUserCookies(ctx, {
+            access_token,
+            refresh_token,
+            session_uid: session.id,
         });
 
-        setUserInRedis({
+        userServices.setUserInRedis({
             ...celebrityUser,
             session: [session],
         });
@@ -111,7 +83,7 @@ export const singinCelebrityUser = async ({ input, ctx }: { input: SigninType; c
     const deviceInfo = useragent.parse(ctx.req.headers['user-agent'] as string);
 
     try {
-        const celebrityUser = await prisma.celebrity.findUnique({
+        const celebrityUser = await userServices.findCelebrityUser({
             where: {
                 email: input.email,
             },
@@ -144,7 +116,7 @@ export const singinCelebrityUser = async ({ input, ctx }: { input: SigninType; c
         const findSessionInUser = celebrityUser.session.find((session) => session.id === device_uid);
 
         if (findSessionInUser) {
-            await prisma.session.delete({
+            await userServices.deleteUserSession({
                 where: {
                     id: device_uid,
                 },
@@ -153,23 +125,20 @@ export const singinCelebrityUser = async ({ input, ctx }: { input: SigninType; c
                 },
             });
 
-            deleteUserSessionInRedis(celebrityUser.username, device_uid);
+            userServices.deleteUserSessionInRedis(celebrityUser.username, device_uid);
 
-            ctx.res.clearCookie('access_token');
-            ctx.res.clearCookie('refresh_token');
-            ctx.res.clearCookie('device_uid');
-            ctx.res.clearCookie('logged_in');
+            userServices.removeUserCookies(ctx);
         }
 
         // Create the Access and refresh Tokens
-        const { access_token, refresh_token } = signTokens({
+        const { access_token, refresh_token } = userServices.signTokens({
             email: celebrityUser.email,
             id: celebrityUser.id,
             username: celebrityUser.username,
         });
 
         //Create sesstion for user agent
-        const session = await prisma.session.create({
+        const session = await userServices.createUserSession({
             data: {
                 refreshToken: refresh_token,
                 userAgent: JSON.stringify(deviceInfo),
@@ -181,20 +150,15 @@ export const singinCelebrityUser = async ({ input, ctx }: { input: SigninType; c
         const sessionIndex = celebrityUser.session.findIndex((data) => data.id === device_uid);
         celebrityUser.session.splice(sessionIndex, 1, session);
 
-        setUserInRedis({
+        userServices.setUserInRedis({
             ...celebrityUser,
         });
 
         //  Send Access Token in Cookie
-        ctx.res.cookie('access_token', access_token, accessTokenCookieOptions);
-        ctx.res.cookie('refresh_token', refresh_token, refreshTokenCookieOptions);
-        ctx.res.cookie('device_uid', session.id, {
-            ...cookieOptions,
-            httpOnly: true,
-        });
-        ctx.res.cookie('logged_in', true, {
-            ...cookieOptions,
-            httpOnly: false,
+        userServices.setUserCookies(ctx, {
+            access_token,
+            refresh_token,
+            session_uid: session.id,
         });
 
         return {
@@ -207,57 +171,6 @@ export const singinCelebrityUser = async ({ input, ctx }: { input: SigninType; c
             cause: error,
             code: 'BAD_REQUEST',
             message: 'BAD REQUEST',
-        });
-    }
-};
-
-const setUserInRedis = (
-    user: Prisma.CelebrityGetPayload<{
-        select: {
-            username: true;
-            email: true;
-            session: true;
-        };
-    }>
-) => {
-    // 1. Create Session
-
-    //TODO : Need to set a expire date after 2 month
-    redisClient.set(`${user.username}`, JSON.stringify({ ...user }));
-};
-
-const deleteUserSessionInRedis = async (username: string, deviceUid: string) => {
-    const jsonUser = (await redisClient.get(username)) as string;
-
-    if (jsonUser) {
-        const user = JSON.parse(jsonUser) as Prisma.CelebrityGetPayload<{
-            select: {
-                session: true;
-            };
-        }>;
-
-        const sessionIndex = user.session.findIndex((data) => data.id === deviceUid);
-        user.session.splice(sessionIndex, 1);
-    }
-};
-
-export const signTokens = (user: { email: string; id: string; username: string | null }) => {
-    try {
-        // 2. Create Access and Refresh tokens
-        const access_token = signJwt(user, 'accessTokenPrivateKey', {
-            expiresIn: `${customConfig.accessTokenExpiresIn}m`,
-        });
-
-        const refresh_token = signJwt(user, 'refreshTokenPrivateKey', {
-            expiresIn: `${customConfig.refreshTokenExpiresIn}m`,
-        });
-
-        return { access_token, refresh_token };
-    } catch (error) {
-        throw new TRPCError({
-            cause: error,
-            code: 'INTERNAL_SERVER_ERROR',
-            message: 'SERVER ERROR',
         });
     }
 };
---
+++ backend/hero/service/user/index.ts
@@ -0,0 +1,135 @@
+import { Prisma } from 'database';
+import prisma from '../../utils/prisma';
+import redisClient from '../../utils/connectRedis';
+import { CookieOptions } from 'express';
+import customConfig from '../../config/default';
+import { Context } from '../../server';
+import { signJwt } from '../../utils/jwt';
+import { TRPCError } from '@trpc/server';
+
+const cookieOptions: CookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+};
+
+const accessTokenCookieOptions = {
+    ...cookieOptions,
+    expires: new Date(Date.now() + customConfig.accessTokenExpiresIn * 24 * 3600000),
+};
+
+const refreshTokenCookieOptions = {
+    ...cookieOptions,
+    expires: new Date(Date.now() + customConfig.refreshTokenExpiresIn * 24 * 3600000),
+};
+
+const createCelebrityUser = async (createArg: Prisma.CelebrityCreateArgs) => {
+    return await prisma.celebrity.create(createArg);
+};
+
+const findCelebrityUser = async <T extends Prisma.CelebritySelect | undefined>({
+    where = {},
+    select,
+}: {
+    select?: T;
+    where?: Prisma.CelebrityWhereUniqueInput;
+}) => {
+    return (await prisma.celebrity.findUnique({
+        where,
+        select,
+    })) as Prisma.CelebrityGetPayload<{ select: T }>;
+};
+
+const createUserSession = async (creaetArg: Prisma.SessionCreateArgs) => {
+    return await prisma.session.create(creaetArg);
+};
+
+const deleteUserSession = async (deleteArg: Prisma.SessionDeleteArgs) => {
+    return await prisma.session.delete(deleteArg);
+};
+
+const setUserInRedis = (
+    user: Prisma.CelebrityGetPayload<{
+        select: {
+            username: true;
+            email: true;
+            session: true;
+        };
+    }>
+) => {
+    // 1. Create Session
+
+    //TODO : Need to set a expire date after 2 month
+    redisClient.set(`${user.username}`, JSON.stringify({ ...user }));
+};
+
+const deleteUserSessionInRedis = async (username: string, deviceUid: string) => {
+    const jsonUser = (await redisClient.get(username)) as string;
+
+    if (jsonUser) {
+        const user = JSON.parse(jsonUser) as Prisma.CelebrityGetPayload<{
+            select: {
+                session: true;
+            };
+        }>;
+
+        const sessionIndex = user.session.findIndex((data) => data.id === deviceUid);
+        user.session.splice(sessionIndex, 1);
+    }
+};
+
+const setUserCookies = (
+    ctx: Context,
+    { access_token, refresh_token, session_uid }: { access_token: string; refresh_token: string; session_uid: string }
+) => {
+    ctx.res.cookie('access_token', access_token, accessTokenCookieOptions);
+    ctx.res.cookie('refresh_token', refresh_token, refreshTokenCookieOptions);
+    ctx.res.cookie('device_uid', session_uid, {
+        ...cookieOptions,
+        httpOnly: true,
+    });
+    ctx.res.cookie('logged_in', true, {
+        ...cookieOptions,
+        httpOnly: false,
+    });
+};
+
+const removeUserCookies = (ctx: Context) => {
+    ctx.res.clearCookie('access_token');
+    ctx.res.clearCookie('refresh_token');
+    ctx.res.clearCookie('device_uid');
+    ctx.res.clearCookie('logged_in');
+};
+
+export const signTokens = (user: { email: string; id: string; username: string | null }) => {
+    try {
+        // 2. Create Access and Refresh tokens
+        const access_token = signJwt(user, 'accessTokenPrivateKey', {
+            expiresIn: `${customConfig.accessTokenExpiresIn}m`,
+        });
+
+        const refresh_token = signJwt(user, 'refreshTokenPrivateKey', {
+            expiresIn: `${customConfig.refreshTokenExpiresIn}m`,
+        });
+
+        return { access_token, refresh_token };
+    } catch (error) {
+        throw new TRPCError({
+            cause: error,
+            code: 'INTERNAL_SERVER_ERROR',
+            message: 'SERVER ERROR',
+        });
+    }
+};
+
+export default {
+    createCelebrityUser,
+    createUserSession,
+    setUserInRedis,
+    deleteUserSessionInRedis,
+    setUserCookies,
+    removeUserCookies,
+    deleteUserSession,
+    findCelebrityUser,
+    signTokens,
+};
