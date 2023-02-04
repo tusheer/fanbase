e847b4b5b7aa7ef7fe1a98e2e432fc4b745a2b7c
tusheer <jaanealamtusher@gmail.com>
2023-02-04 15:47:30 +0600

    add auth middleware in image upload

--- backend/hero/router/upload/index.ts
+++ backend/hero/router/upload/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import uploader from '../../utils/upload';
 import { singleImageUploadController } from '../../controller/upload';
 import errorHandler from '../../middleware/errorHandler';
+import authMiddleware from '../../middleware/authExpressMiddleware';
 
 const uploadRouter = express();
 
 uploadRouter.post(
     '/image',
+    authMiddleware,
     uploader(['image/jpeg', 'image/webp'], 1048576, 'Image allow only').single('image'),
     singleImageUploadController
 );
