8f974677d47778b7a4ea7a73f5d408a83a5d1c19
tusheer <jaanealamtusher@gmail.com>
2023-02-14 14:20:49 +0600

    add blur hash utils

--- frontend/celebrity-portal/src/modules/profile/components/UploadProfileModal/index.tsx
+++ frontend/celebrity-portal/src/modules/profile/components/UploadProfileModal/index.tsx
@@ -23,15 +23,17 @@ const UploadProfileModal: React.FC<IProfileImageUploadProps> = ({ isOpen, onClos
                     <FileUploadInput
                         multiple={false}
                         accept="image/*"
-                        className="mx-auto block h-64 w-64 rounded-full bg-red-300"
+                        className="mx-auto block h-64 w-64 rounded-full"
                         onChange={onChange}
                     >
-                        <div className="relative mx-auto h-64 w-64 overflow-hidden rounded-full  border border-gray-100 bg-gray-200">
+                        <div className="relative mx-auto h-64 w-64 overflow-hidden rounded-full  border border-gray-100">
                             <Image
                                 className="absolute h-full w-full object-cover"
                                 fill={true}
                                 alt=""
+                                placeholder="blur"
                                 src={files[0]?.url || '/images/profile/blank_profile.png'}
+                                blurDataURL="LEHV6nWB2yk8pyo0adR*.7kCMdnj"
                             />
                         </div>
                     </FileUploadInput>
