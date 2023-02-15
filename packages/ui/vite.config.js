db826c6b1ace0d3d54099519cd15af155a6a6b70
tusheer <jaanealamtusher@gmail.com>
2023-02-15 12:37:46 +0600

    add vitest for ttd

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -1,8 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const react = require('@vitejs/plugin-react');
 const { defineConfig } = require('vite');
+const { configDefaults } = require('vitest/config');
 // const tsconfigPaths = require("vite-tsconfig-paths");
 
 module.exports = defineConfig({
     plugins: [react()],
+    test: {
+        exclude: [...configDefaults.exclude, 'components/*.{test,spec}.{ts,tsx}', "hooks/*.{test,spec}.{ts,tsx}", "utils/*.{test,spec}.{ts,tsx}"],
+        globals: true,
+        environment: 'happy-dom',
+    },
 });
