b2ff1e1561da38b922533d147720350429ebf645
tusheer <jaanealamtusher@gmail.com>
2023-02-17 09:18:33 +0600

    fix typescript config issue

--- packages/eslint-config-custom-react/index.js
+++ packages/eslint-config-custom-react/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: ["@typescript-eslint"],
-  extends: ['turbo', 'prettier', "eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking"],
+  extends: ['turbo', 'prettier', "eslint:recommended"],
   rules: {
     "no-console": "error",
     "@typescript-eslint/no-unused-vars": "error",
