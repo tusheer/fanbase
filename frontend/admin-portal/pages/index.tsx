7e117b2c48f7b9e223fc35c75e415b0d018f145e
tusheer <jaanealamtusher@gmail.com>
2023-03-09 08:38:41 +0600

    remove @next/font

--- frontend/admin-portal/pages/index.tsx
+++ frontend/admin-portal/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Inter } from '@next/font/google';
+import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
